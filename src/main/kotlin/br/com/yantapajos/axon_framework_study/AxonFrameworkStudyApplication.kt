1702f0aea5de89f6eb41635747d2bc7822e23135
Yan Brandão <ybrandao.d@gmail.com>
2025-01-01 22:22:45 +0000

    Add project from spring.io

---
+++ src/main/kotlin/br/com/yantapajos/axon_framework_study/AxonFrameworkStudyApplication.kt
@@ -0,0 +1,11 @@
+package br.com.yantapajos.axon_framework_study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AxonFrameworkStudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<AxonFrameworkStudyApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/yantapajos/axon_framework_study/AxonFrameworkStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.yantapajos.axon_framework_study
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AxonFrameworkStudyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
